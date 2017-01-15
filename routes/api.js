8f61e568e5fac16bd0ea4e2fd2c8ee006904b869
bxmmm <benjaminxh@gmail.com>
2017-01-15 16:44:27 +0100

    asd

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,14 @@ var express = require('express'),
      router = express.Router();
  var middle = require('../middleware/index');
 
-router.get('/articles', function(req, res){
-    // find articles in DB
+router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+router.get('/articles', function(req, res, next){
+    // find articles in DB
     Article.find({}, function(err, foundArticles){
         if (err) {
             console.log(err)
