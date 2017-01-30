c0dce7886b066890a2172b84ca968b70faf2c979
bxmmm <benjaminxh@gmail.com>
2017-01-30 11:42:01 +0100

    fixnecega

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,8 @@ var express = require('express'),
      router = express.Router();
  var middle = require('../middleware/index');
 
+
+// origin za api request 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
